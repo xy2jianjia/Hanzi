60f88c4e53a1730fe00c121ce31447a7d45cebf1
xy2jianjia <xy2jianjia@163.com>
2015-11-22 09:54:23 +0800

    Initial Commit

---
+++ Hanzi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hanzi
+//
+//  Created by sincsmart on 15/11/22.
+//  Copyright © 2015年 hanzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HanziTests/HanziTests.m
@@ -0,0 +1,39 @@
+//
+//  HanziTests.m
+//  HanziTests
+//
+//  Created by sincsmart on 15/11/22.
+//  Copyright © 2015年 hanzi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HanziTests : XCTestCase
+
+@end
+
+@implementation HanziTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
